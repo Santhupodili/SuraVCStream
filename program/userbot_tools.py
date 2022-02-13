f10521c9092104e49fa8e968cbf0951789f3d65a
SANTHOSHPODILI <91259967+SANTHOSHPODILI@users.noreply.github.com>
2022-02-13 19:12:12 +0530

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -1,5 +1,5 @@
 import asyncio
-from driver.surabot import user
+from driver.santhubot import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
