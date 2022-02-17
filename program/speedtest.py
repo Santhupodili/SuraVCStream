ee169a2672354e62d79a87c6f0172c58aa586e73
Santhupodili <99010597+Santhupodili@users.noreply.github.com>
2022-02-17 18:57:53 +0530

    Update speedtest.py

--- program/speedtest.py
+++ program/speedtest.py
@@ -8,7 +8,7 @@ from program.utils.formatters import bytes
 from driver.filters import command, other_filters
 from driver.decorators import sudo_users_only
 from config import BOT_USERNAME as bname
-from driver.surabot import bot as app
+from driver.santhubot import bot as app
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
@@ -16,35 +16,35 @@ from pyrogram.types import Message
 @Client.on_message(command(["speedtest", f"speedtest@{bname}"]) & ~filters.edited)
 @sudo_users_only
 async def run_speedtest(_, message: Message):
-    m = await message.reply_text("⚡️ running server speedtest")
+    m = await message.reply_text("💘 ʀᴜɴɴɪɴɢ sᴇʀᴠᴇʀ sᴘᴇᴇᴅᴛᴇsᴛ")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = await m.edit("⚡️ running download speedtest..")
+        m = await m.edit("😁 ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴘᴇᴇᴅᴛᴇsᴛ..")
         test.download()
-        m = await m.edit("⚡️ running upload speedtest...")
+        m = await m.edit("💚 ʀᴜɴɴɪɴɢ ᴜᴘʟᴏᴀᴅ sᴘᴇᴇᴅᴛᴇsᴛ...")
         test.upload()
         test.results.share()
         result = test.results.dict()
     except Exception as e:
         await m.edit(e)
         return
-    m = await m.edit("🔄 sharing speedtest results")
+    m = await m.edit("❤ sʜᴀʀɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs")
     path = wget.download(result["share"])
 
-    output = f"""💡 **SpeedTest Results**
+    output = f"""🎉 **sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs**
     
-<u>**Client:**</u>
-**ISP:** {result['client']['isp']}
-**Country:** {result['client']['country']}
+<u>**ᴄʟɪᴇɴᴛ:**</u>
+**ɪsᴘ:** {result['client']['isp']}
+**ᴄᴏᴜɴᴛʀʏ:** {result['client']['country']}
   
-<u>**Server:**</u>
-**Name:** {result['server']['name']}
-**Country:** {result['server']['country']}, {result['server']['cc']}
-**Sponsor:** {result['server']['sponsor']}
-**Latency:** {result['server']['latency']}
+<u>**sᴇʀᴠᴇʀ:**</u>
+**ɴᴀᴍᴇ:** {result['server']['name']}
+**ᴄᴏᴜɴᴛʀʏ:** {result['server']['country']}, {result['server']['cc']}
+**sᴘᴏɴsᴏʀ:** {result['server']['sponsor']}
+**ʟᴀᴛᴇɴᴄʏ:** {result['server']['latency']}
 
-⚡️ **Ping:** {result['ping']}"""
+💘 **ᴘɪɴɢ:** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=path, caption=output
     )
