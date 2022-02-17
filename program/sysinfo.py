77e15f17ec8009c270239413cf7a68fef6c9bf3e
Santhupodili <99010597+Santhupodili@users.noreply.github.com>
2022-02-17 18:00:45 +0530

    Update sysinfo.py

--- program/sysinfo.py
+++ program/sysinfo.py
@@ -35,7 +35,7 @@ async def give_sysinfo(client, message):
     psutil.disk_io_counters()
     disk = f"{humanbytes(du.used)} / {humanbytes(du.total)} " f"({du.percent}%)"
     cpu_len = len(psutil.Process().cpu_affinity())
-    somsg = f"""🖥 **System Information**
+    somsg = f"""💖 **sʏsᴛᴇᴍ ɪɴғᴏʀᴍᴀᴛɪᴏɴ**
     
 **PlatForm :** `{splatform}`
 **PlatForm - Release :** `{platform_release}`
